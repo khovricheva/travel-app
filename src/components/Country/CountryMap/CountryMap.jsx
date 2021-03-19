cb45acdff0860aee94519f7745d3a49b8ec07704
khovricheva <kseniyap1102@gmail.com>
2021-03-19 15:17:26 +0200

    feat: add fullscreen

--- src/components/Country/CountryMap/CountryMap.jsx
+++ src/components/Country/CountryMap/CountryMap.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './CountryMap.scss';
 import { MapContainer, TileLayer, Marker, Popup, Polygon } from 'react-leaflet';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+import Button from '@material-ui/core/Button';
 
 function CountryMap({ lat, lon, capital, geoCoordinates }) {
   const [polygon, setPolygon] = useState([]);
+  const handle = useFullScreenHandle();
 
   useEffect(() => {
     if (geoCoordinates.length < 5) {
@@ -17,31 +20,36 @@ function CountryMap({ lat, lon, capital, geoCoordinates }) {
 
   return (
     <div className='countryMap' id='mapid'>
-      <MapContainer
-        center={[lat, lon]}
-        zoom={5}
-        scrollWheelZoom={false}
-        className='mapContainer'
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url={
-            'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
-          }
-        />
-        <Polygon
-          positions={polygon}
-          stroke={false}
-          fillColor={'lime'}
-          weight={2}
-          lineCap='round'
-          lineJoin='round'
-        />
+      <Button variant='contained' color='primary' onClick={handle.enter}>
+        Fullscreen map
+      </Button>
+      <FullScreen handle={handle}>
+        <MapContainer
+          center={[lat, lon]}
+          zoom={5}
+          scrollWheelZoom={false}
+          className='mapContainer'
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url={
+              'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+            }
+          />
+          <Polygon
+            positions={polygon}
+            stroke={false}
+            fillColor={'lime'}
+            weight={2}
+            lineCap='round'
+            lineJoin='round'
+          />
 
-        <Marker position={[lat, lon]}>
-          <Popup>{capital}</Popup>
-        </Marker>
-      </MapContainer>
+          <Marker position={[lat, lon]}>
+            <Popup>{capital}</Popup>
+          </Marker>
+        </MapContainer>
+      </FullScreen>
     </div>
   );
 }
