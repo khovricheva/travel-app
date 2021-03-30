6aead047091e255af6bfb5ae195939d69f930bcd
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-03-30 21:55:55 +0300

    add darkmode

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import './Countries.scss';
 import { CountryContext } from '../../context/GlobalContext';
 import CountryCards from './CountryCards';
-import ErrorFallback from '../ErrorBoundary/ErrorBoundary';
+import ErrorFallback from '../ErrorBoundary';
 import { ErrorBoundary } from 'react-error-boundary';
 
 const Countries = () => {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,17 @@ import Input from './Input';
 import Language from './Language';
 import Title from './Title';
 import { useLocation } from 'react-router-dom';
+import DarkMode from './DarkMode';
 
 const Header = () => {
-  
   const location = useLocation();
 
   return (
     <header className="header">
       <Title />
-      {!location.pathname.includes('country') ? <Input /> : null}
-      <Language />
+        {!location.pathname.includes('country') ? <Input /> : null}
+        <Language />
+        <DarkMode />
     </header>
   );
 };
