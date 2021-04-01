beb745e1d399545cd84c2af7cdbe24f4b999ce39
khovricheva <kseniyap1102@gmail.com>
2021-04-01 15:43:15 +0300

    refactor: change Spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,8 +3,13 @@ import './Spinner.scss';
 
 const Spinner = () => {
   return (
-    <div className="wrapper">
-      <div className="cssload-loader"></div>
+    <div className='wrapper'>
+      <div class='loadingio-spinner-ripple'>
+        <div class='ldio'>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
