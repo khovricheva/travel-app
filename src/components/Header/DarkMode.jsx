5e89f1d92ddd97ee98e3172dc80a2c0762660455
khovricheva <kseniyap1102@gmail.com>
2021-04-01 15:28:39 +0300

    feat: implement DarkMode and add colors scss

--- src/components/Header/DarkMode.jsx
+++ src/components/Header/DarkMode.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DarkModeToggle from 'react-dark-mode-toggle';
 
 const DarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    JSON.parse(localStorage.getItem('DARK_MODE'))
+  );
+
+  const addClass = (isDarkMode) => {
+    isDarkMode
+      ? document.body.classList.add('dark-mode')
+      : document.body.classList.remove('dark-mode');
+  };
+
+  useEffect(() => {
+    addClass(isDarkMode);
+  }, [isDarkMode]);
+
+  const handleModeChange = () => {
+    addClass(isDarkMode);
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem('DARK_MODE', !isDarkMode);
+  };
+
   return (
     <DarkModeToggle
-      onChange={setIsDarkMode}
+      onChange={handleModeChange}
       checked={isDarkMode}
       size={80}
       className='darkMode'
