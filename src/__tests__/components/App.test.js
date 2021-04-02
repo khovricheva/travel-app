421c5d31938594c38a91f464087dc185bbd41f9e
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-02 16:26:16 +0300

    update tests

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import App from '../../App'
- 
- it('should take a snapshot', () => {
-    const { asFragment } = render(<App />)
-    
-    expect(asFragment(<App />)).toMatchSnapshot()
-   })
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../../App';
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/another-route',
+  }),
+}));
+
+describe('App component', () => {
+  it('Renders correctly', () => {
+    const wrapper = render(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
