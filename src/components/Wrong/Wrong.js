944577f29c9b5b5c9c7aa9ac085cca080fbf1095
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-05 17:39:32 +0300

    update some styles

--- src/components/Wrong/Wrong.js
+++ src/components/Wrong/Wrong.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import translate from '../../translate';
-
+import '../NotFound/NotFound.scss'
 const Wrong = () => {
   const code = useSelector((state) => state.code);
 
   return (
-    <div>
-      <h1>{translate.wrong[code]}</h1>
-      <Link to='/'>
-        <input type='submit' value={translate.wrong.button[code]} />
+    <div className="notFound">
+      <h1 className="notFoundText">{translate.wrong[code]}</h1>
+      <Link to="/">
+        <input
+          className="notFoundBtn"
+          type="submit"
+          value={translate.wrong.button[code]}
+        />
       </Link>
     </div>
   );
