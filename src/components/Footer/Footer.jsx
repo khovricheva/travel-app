10beb3cdb6cf5ae6e11ff51c52d72a97633ba1a1
ArtemSirobaba <59284335+ArtemSirobaba@users.noreply.github.com>
2021-04-08 22:41:02 +0300

    delete logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
-import './Footer.scss';
 import { Typography, Link, Box } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Footer = () => {
   return (
     <footer>
-      <Box align="center" display="flex" justifyContent="space-around">
+      <Box align="center" display="flex" justifyContent="space-around" paddingBottom='10px'>
         <Typography>
           <GitHubIcon></GitHubIcon>
           <Link href="https://github.com/ArtemSirobaba">@ArtemSirobaba</Link>
         </Typography>
-        <Link href="https://rs.school/js/">
-          <img
-            className="school-logo"
-            alt="school-logo"
-            src="https://rs.school/images/rs_school_js.svg"
-          />
-        </Link>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          {new Date().getFullYear()}
+        </Typography>
         <Typography>
           <GitHubIcon></GitHubIcon>
           <Link href="https://github.com/khovricheva">@khovricheva</Link>
         </Typography>
       </Box>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="textSecondary"
-        component="p"
-      >
-        {new Date().getFullYear()}
-      </Typography>
     </footer>
   );
 };
